2cc6e43a2e2a7f49e5c38425a3c247a8cee34426
Baptiste Rajaut <baptiste@rajaut.fr>
2025-05-02 11:15:45 +0200

    better log cleaner

--- utils/cleanhoi4logs.sh
+++ utils/cleanhoi4logs.sh
@@ -1,5 +1,17 @@
 #!/bin/bash 
-set -ex
+set -e
+FORCE=0
+if [ "$1" == "-f" ]; then
+    FORCE=1
+fi
+
+# Check if the 'hoi4' process is running
+if pgrep -x "hoi4" > /dev/null && [ "$FORCE" -ne 1 ]; then
+    echo "Cannot clean the log while 'hoi4' is running."
+    echo "Cleaning the error log while the game is running prevents it from logging properly."
+    echo "Use '-f' to force the clean anyway (at your own risk)."
+    exit 1
+fi
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
